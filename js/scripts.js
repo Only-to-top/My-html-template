d4aa21688ec0980ddaef28c9c2f1b7ae54d395d3
Nikolay <32436118+SoldatovNikolay@users.noreply.github.com>
2018-11-14 22:00:59 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(function() {
 
 	// $('.element').equalHeights();
+	
+	$("#element, #element2").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+		top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 
 });
