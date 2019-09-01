d6ae0ce5b37d6a85ab3794a3a19fbd3ba1d1275a
Nikolay <32436118+SoldatovNikolay@users.noreply.github.com>
2019-09-01 17:23:29 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,104 @@
 jQuery(function() {
   
-  $(document).on('click', '.hamburger', function() {
-    $(this).toggleClass('is-active');
-  });
-  
-  $('[data-fancybox=""]').fancybox({
-    animationEffect: 'zoom-in-out',
-  });
-
-  //E-mail Ajax Send
-  $(document).on('submit', '.main-form', function() {
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php",
-      data: th.serialize()
-    }).done(function() {
-      // alert("Спасибо! Сообщение отправлено!");
-      swal({ title: 'Сообщение отправлено', type: 'success' });
-      setTimeout(function() {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
+    $(document).on('click', '.hamburger', function() {
+      $(this).toggleClass('is-active');
     });
-    return false;
-  });
-  
-  $('.btn-send-form').on('click', function(){
-    if ( $('form input[type=checkbox]').is(':checked') ) {
-      $('form .checkbox-new').removeClass('red');
-    } else
-      $('form .checkbox-new').addClass('red');
-  });
 
-  // Открыть всплывающее окно с формой
-  $('.popup').click(function(){
-    $('.popup-wrap').fadeIn();
-    $(".popup-wrap .title").html($(this).text()); //текст ссылки вставляем в название модального окна
-  });
-  
-  if ( $(window).width() < 768 ) {
-    
-    $('.class').equalHeights();
-
-    //Скрыть блок при клике вне его
-    $(document).mouseup(function (e) {
-        var container = $(".search--show");
-        if (container.has(e.target).length === 0){
-            container.fadeOut();
-        };
+    $('[data-fancybox=""]').fancybox({
+      animationEffect: 'zoom-in-out',
     });
-    
-  }
 
-
-  // slider
-  var swiper = new Swiper('.header-swiper-container', {
-      navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', },
-      pagination: { el: '.swiper-pagination', clickable: true, dynamicBullets: true, },
-      autoplay: { delay: 3500, disableOnInteraction: false, },
-      spaceBetween: 30, // расст-е м-у слайдами
-      grabCursor: true, // рука
-      loop: true,
-      mousewheel: true, // прокрутка колесом
-      slidesPerView: 2, // кол-во слайдов
-      // effect: 'fade', // для slidesPerView: 1
-      // centeredSlides: true, //центрировать
-      // lazy: true,
-      breakpoints: {
-        1200: { // < 1200
-          slidesPerView: 2,
-          spaceBetween: 40,
-        },
-        992: { // < 1024
-          slidesPerView: 1,
-          spaceBetween: 40,
-        },
-        768: { // < 768
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        576: { // < 768
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-      }
-  });
+    //E-mail Ajax Send
+    $(document).on('submit', '.main-form', function() {
+      var th = $(this);
+      $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: th.serialize()
+      }).done(function() {
+        // alert("Спасибо! Сообщение отправлено!");
+        swal({ title: 'Сообщение отправлено', type: 'success' });
+        setTimeout(function() {
+          // Done Functions
+          th.trigger("reset");
+        }, 1000);
+      });
+      return false;
+    });
   
+    $('.btn-send-form').on('click', function(){
+      if ( $('form input[type=checkbox]').is(':checked') ) {
+        $('form .checkbox-new').removeClass('red');
+      } else
+        $('form .checkbox-new').addClass('red');
+    });
+
+    // Открыть всплывающее окно с формой
+    $('.popup').click(function(){
+      $('.popup-wrap').fadeIn();
+      $(".popup-wrap .title").html($(this).text()); //текст ссылки вставляем в название модального окна
+    });
+  
+    if ( $(window).width() < 768 ) {
+
+      $('.class').equalHeights();
+
+      //Скрыть блок при клике вне его
+      $(document).mouseup(function (e) {
+          var container = $(".search--show");
+          if (container.has(e.target).length === 0){
+              container.fadeOut();
+          };
+      });
+
+    }
+
+
+    // swiper slider
+    var swiper = new Swiper('.header-swiper-container', {
+        navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', },
+        pagination: { el: '.swiper-pagination', clickable: true, dynamicBullets: true, },
+        autoplay: { delay: 3500, disableOnInteraction: false, },
+        spaceBetween: 30, // расст-е м-у слайдами
+        grabCursor: true, // рука
+        loop: true,
+        mousewheel: true, // прокрутка колесом
+        slidesPerView: 2, // кол-во слайдов
+        // effect: 'fade', // для slidesPerView: 1
+        // centeredSlides: true, //центрировать
+        // lazy: true,
+        breakpoints: {
+          1200: { // < 1200
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          992: { // < 1024
+            slidesPerView: 1,
+            spaceBetween: 40,
+          },
+          768: { // < 768
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          576: { // < 768
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        }
+    });
+  
+    // stop autoplay swiper on hover
     $(".header-swiper-container").hover(function() {
         (this).swiper.autoplay.stop();
     }, function() {
         (this).swiper.autoplay.start();
     });
 
-  
-  $("#element, #element2").on("click", "a", function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href'), top = $(id).offset().top;
-    $('body, html').animate({scrollTop: top}, 1500);
-  });
+    // scroll to top
+    $("#element, #element2").on("click", "a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'), top = $(id).offset().top;
+      $('body, html').animate({scrollTop: top}, 1500);
+    });
 
 });
