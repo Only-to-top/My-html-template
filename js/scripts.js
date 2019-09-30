fbad52d2ed23dfa85ac15098e8f8b71d5957d1e3
Nikolay <32436118+Only-to-top@users.noreply.github.com>
2019-09-30 17:20:26 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,28 @@ jQuery(function($) {
             };
         });
     };
+    
+    
+    // wow
+    const menuBtn = $('.btn'),
+          menu    = $('.menu');
+
+    menuBtn.on('click', function() {
+        if ( $(this).hasClass('is-active') ) {
+            $(this).removeClass('is-active');
+            menu.slideUp();
+        } else {
+            $(this).addClass('is-active');
+            menu.slideDown();
+        }
+    });
+
+    $(document).click(function (e) {
+        if ( !menuBtn.is(e.target) && !menu.is(e.target) && menu.has(e.target).length === 0) {
+            menu.slideUp();
+            menuBtn.removeClass('is-active');
+        };
+    });
 
 
     // popup
